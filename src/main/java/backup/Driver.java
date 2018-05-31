0d8100fad488c9bb9c9a5a6e709195001ffe74e5
cdjohnnatha <cdjohnnatha@gmail.com>
2018-05-31 01:41:55 -0300

    remove files from root, create maven project, change files from root to folder backup

---
+++ src/main/java/backup/Driver.java
@@ -0,0 +1,36 @@
+//package backup;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class Driver {
+//	private ArrayList<City> initialCities = new ArrayList<City>(Arrays.asList(
+//			new City("Boston", 42.3601, -71.0589),
+//			new City("Houston", 29.7604, -95.3698),
+//			new City("Austin", 30.2672, -97.7431),
+//			new City("San Francisco", 37.7749, -122.4194),
+//			new City("Denver", 39.7392, -104.9903),
+//			new City("Los Angeles", 34.0522, -118.2437),
+//			new City("Chicago", 41.8781, -87.6298),
+//			new City("New York", 40.7128, -74.0059),
+//			new City("Dallas", 32.7767, -96.7970),
+//			new City("Seatle", 47.6062, -122.3321)
+//
+//	));
+//
+//	public static void main(String[] args) {
+//		Driver driver = new Driver();
+//		ArrayList<City> cities = new ArrayList<City>();
+//		cities.addAll(driver.initialCities);
+//		driver.printShortestRoute(new NearestNeighbor().findShortestRoute(cities));
+//
+//	}
+//	private void printShortestRoute(Route shortestRoute) {
+//		System.out.println("---------------------------------------------------------------------------");
+//		System.out.println("Shoutest Route found so far: " + shortestRoute);
+//		System.out.println("w/ total distance: " + shortestRoute.calculateTotalDistance());
+//		System.out.println("---------------------------------------------------------------------------");
+//
+//    }
+//
+//}
---
+++ src/main/java/backup/NearestNeighbor.java
@@ -0,0 +1,37 @@
+//package backup;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class NearestNeighbor {
+//	public Route findShortestRoute(ArrayList<City> cities) {
+//		ArrayList<City> shortestRouteCities = new ArrayList<City>(cities.size());
+//		System.out.println("---------------------------------------------------------------------------");
+//		System.out.println("Initial Route        => " + Arrays.toString(cities.toArray()));
+//		System.out.println("w/ total distance: " + new Route(cities).calculateTotalDistance());
+//		System.out.println("---------------------------------------------------------------------------");
+//		City city = cities.get((int)(cities.size() * Math.random()));
+//		updateRoutes(shortestRouteCities, cities, city);
+//		while (cities.size() >= 1) {
+//			city = getNextCity(cities, city);
+//			updateRoutes(shortestRouteCities, cities, city);
+//		}
+//
+//		return new Route(shortestRouteCities);
+//	}
+//	private void updateRoutes(ArrayList<City> shortestRouteCities, ArrayList<City> cities, City city) {
+//		shortestRouteCities.add(city);
+//		cities.remove(city);
+//		System.out.println("Cities In Shortest Route ==> " + Arrays.toString(shortestRouteCities.toArray()));
+//		System.out.println("Remaining Cities         ==> " + Arrays.toString(cities.toArray())+ "\n");
+//	}
+//	private City getNextCity(ArrayList<City> cities, City city) {
+//		return cities.stream().min((city1, city2) -> {
+//			int flag = 0;
+//			if(city1.measureDistance(city) < city2.measureDistance(city)) flag = -1;
+//			else if(city1.measureDistance(city) > city2.measureDistance(city)) flag = 1;
+//			return flag;
+//		}).get();
+//
+//	}
+//}
